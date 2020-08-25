bf8f476c7d61ffe65926bc36a52e66521c61fac5
thinhtpt2000 <tthinh2128@gmail.com>
2020-08-25 20:25:11 +0700

    add post toolbar to storybook

---
+++ src/stories/PostsTool.stories.js
@@ -0,0 +1,13 @@
+// PostsTool.stories.js
+
+import React from "react";
+import PostsTool from "../components/PostsTool";
+
+export default {
+  title: "Component/PostsTool",
+  component: PostsTool,
+}
+
+const Template = () => <PostsTool />;
+
+export const Default = Template.bind({});
